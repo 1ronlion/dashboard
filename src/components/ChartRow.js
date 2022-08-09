94c39ee8cf6fe5688333df0c04734a296029cddd
1ronlion <leonardo_bana@live.com.ar>
2022-08-09 19:43:57 -0300

    chart row

--- src/components/ChartRow.js
+++ src/components/ChartRow.js
@@ -3,8 +3,18 @@ import React from "react";
 function ChartRow (props) {
 return(
 
-    <>
-    </>
+    <React.Fragment>
+
+        <tr>
+        <td>{props.Title}</td>
+        <td>{props.Length}</td>
+        <td>{props.Rating}</td>
+        <td>{props.Categories}</td>
+        <td>{props.Awards}</td>
+        </tr>
+
+
+    </React.Fragment>
 
 
 )
